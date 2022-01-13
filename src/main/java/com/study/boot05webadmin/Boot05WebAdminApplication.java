78af2b91c1965c772b8198eb8a7d8da9292966e9
baozi <794575081@qq.com>
2022-01-13 21:23:40 +0800

    hot-fix first commit

--- src/main/java/com/study/boot05webadmin/Boot05WebAdminApplication.java
+++ src/main/java/com/study/boot05webadmin/Boot05WebAdminApplication.java
@@ -8,7 +8,9 @@ public class Boot05WebAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot05WebAdminApplication.class, args);
-        System.out.println("adfasdf");
+        System.out.println("adfasdf1");
+        System.out.println("adfasdf2");
+        System.out.println("adfasdf3");
     }
 
 }
